1f9acc927eec9e2a69bb90c0834264c8c39247f5
Martín Montes <martinmp@ext.inditex.com>
2019-12-21 18:31:35 +0100

    feat(front): SSR setup

---
+++ packages/front/src/server/index.js
@@ -0,0 +1,27 @@
+import path from "path";
+import fs from "fs";
+import express from "express";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import ContainerApp from "../app/App";
+
+const PORT = process.env.ME_FRONT_PORT || 8080;
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/*", (req, res) => {
+  const containerHtml = ReactDOMServer.renderToString(<ContainerApp />);
+  const index = path.resolve("./public/index.html");
+  fs.readFile(index, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    return res.send(data.replace('<div id="root"></div>', `<div id="root">${containerHtml}</div>`));
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
---
+++ packages/front/webpack.server.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const webpackNodeExternals = require("webpack-node-externals");
+const baseConfig = require("./webpack.base.js");
+
+const config = {
+  target: "node",
+  mode: "production",
+  entry: "./src/server/index.js",
+  externals: [webpackNodeExternals()],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
+
+module.exports = merge(baseConfig, config);
