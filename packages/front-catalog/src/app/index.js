f78df34fe8aeae4849933293fa7a9ef16302109a
Martín Montes <martinmp@ext.inditex.com>
2019-12-28 16:59:51 +0100

    feat(front): providing unique IDs for microfrontends mounting nodes

--- packages/front-catalog/src/app/index.js
+++ packages/front-catalog/src/app/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/App";
 
-ReactDOM.hydrate(<App />, document.getElementById("root"));
+ReactDOM.hydrate(<App />, document.getElementById("catalog-root"));
--- packages/front-catalog/src/server/helpers/ssr.js
+++ packages/front-catalog/src/server/helpers/ssr.js
@@ -15,7 +15,7 @@ export const render = () => {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
       </head>
       <body>
-        <div id="root">${app}</div>
+        <div id="catalog-root">${app}</div>
         <script src="${config.FRONT_CATALOG_URL}/bundle.js"></script>
       </body>
     </html>
