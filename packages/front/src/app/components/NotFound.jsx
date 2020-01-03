cc6f9aa16decaa8c0ac7995e1adc6944d7deda9e
Martín Montes <martinmp@ext.inditex.com>
2020-01-03 20:28:50 +0100

    feat(front): markup for not found component

--- packages/front/src/app/components/NotFound.jsx
+++ packages/front/src/app/components/NotFound.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { Grid, Icon, Message, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-const NotFound = () => <p>Not found</p>;
+const { Column } = Grid;
+const { Content } = Message;
+
+const GlobalStyle = createGlobalStyle`
+  #root {
+    height: 100%;
+  }
+`;
+const StyledGrid = styled(Grid)`
+  height: 100%;
+  background-color: whitesmoke !important;
+`;
+const StyledHeader = styled(Message.Header)`
+  margin-bottom: 1em !important;
+`;
+
+const NotFound = () => (
+  <>
+    <GlobalStyle />
+    <StyledGrid verticalAlign="middle">
+      <Column textAlign="center">
+        <Grid stackable columns={3} centered>
+          <Column textAlign="center">
+            <Message color="red" icon>
+              <Icon name="search" />
+              <Content>
+                <StyledHeader>Oops! We couldn't find what you was looking for.</StyledHeader>
+                <Button as={Link} to="/">
+                  <Icon name="cubes" /> Go Home
+                </Button>
+              </Content>
+            </Message>
+          </Column>
+        </Grid>
+      </Column>
+    </StyledGrid>
+  </>
+);
 
 export default NotFound;
