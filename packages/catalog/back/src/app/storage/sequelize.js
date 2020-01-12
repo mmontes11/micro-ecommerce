70021d43c11d9c84fafd1b5e060a96ef30a2249f
Martín Montes <martinmp@ext.inditex.com>
2020-01-12 16:10:02 +0100

    feat(back-catalog): support for sqlite

--- packages/catalog/back/src/app/storage/sequelize.js
+++ packages/catalog/back/src/app/storage/sequelize.js
@@ -1,16 +1,37 @@
 import Sequelize from "sequelize";
 import logger from "../../helpers/log";
 
+let opts = {
+  dialect: process.env.BACK_CATALOG_DB_DIALECT,
+  logging: msg => logger.info(msg),
+};
+
+switch (opts.dialect) {
+  case "mariadb": {
+    opts = {
+      ...opts,
+      host: process.env.BACK_CATALOG_DB_HOST,
+      port: process.env.BACK_CATALOG_DB_PORT,
+    };
+    break;
+  }
+  case "sqlite": {
+    opts = {
+      ...opts,
+      storage: process.env.BACK_CATALOG_DB_STORAGE,
+    };
+    break;
+  }
+  default: {
+    throw new Error(`Invalid database dialect: '${opts.dialect}'`);
+  }
+}
+
 const sequelize = new Sequelize(
   process.env.BACK_CATALOG_DB_NAME,
   process.env.BACK_CATALOG_DB_USER,
   process.env.BACK_CATALOG_DB_PASSWORD,
-  {
-    host: process.env.BACK_CATALOG_DB_HOST,
-    port: process.env.BACK_CATALOG_DB_PORT,
-    dialect: "mariadb",
-    logging: msg => logger.info(msg),
-  },
+  opts,
 );
 
 export default sequelize;
