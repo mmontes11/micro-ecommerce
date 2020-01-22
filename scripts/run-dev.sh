51cc8b2372f4077c54c8649400b493bb51ab6624
Martín Montes <martinmp@ext.inditex.com>
2020-01-22 18:43:27 +0100

    Added nvm and improved lerna support

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -3,8 +3,16 @@
 # Requirements
 # ttab: https://github.com/mklement0/ttab
 
+set -e
+
 echo "Starting... 🚀"
+
+[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
+nvm install
+nvm use
+
 docker-compose -f docker-compose.dev.yml up -d
-ttab -t "back-catalog" "cd packages/catalog/back; npm start"
-ttab -t "front" "cd packages/front; npm start"
-ttab -t "front-catalog" "cd packages/catalog/front; npm start"
+
+ttab -t "back-catalog" "cd packages/catalog/back; nvm use; npm start"
+ttab -t "front" "cd packages/front; nvm use; npm start"
+ttab -t "front-catalog" "cd packages/catalog/front; nvm use; npm start"
